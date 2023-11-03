1bc5387a387f560613e745f8eda99dd5bfe89b16
mohamad <inf.mhussain@googlemail.com>
2023-11-03 11:35:22 +0100

    initial commit

---
+++ src/test/java/com/mhussain/helloyaml/HelloYamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhussain.helloyaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloYamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
